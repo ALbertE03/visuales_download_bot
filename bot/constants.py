b86a553f1a6e9674f735120c68b4d995aee1fe9e
ALbertE03 <marichalalberto292@gmail.com>
2026-04-21 03:00:27 -0400

    fix yt-dl

--- bot/constants.py
+++ bot/constants.py
@@ -126,6 +126,17 @@ class CONSTANTS:
         "format": "bestvideo+bestaudio/best",
         "quiet": True,
         "no_warnings": True,
+        "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-us,en;q=0.5",
+            "Sec-Fetch-Mode": "navigate",
+        },
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android", "web"]
+            }
+        }
     }
     
     # Dominios soportados por yt-dlp
